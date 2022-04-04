c763698901014cac914251c5fae425d508ff05c9
naveensurya-2000 <naveens19032000@gmail.com>
2022-04-04 12:07:22 +0530

    FIRST COMMIT

---
+++ OrderItemWebApi/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderItemWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace OrderItemWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public Cart Post(int menuItemId)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://20.88.191.182/api/");
+                var response = client.GetAsync($"MenuItem/{menuItemId}");
+                response.Wait();
+                var res = response.Result;
+                var list = res.Content.ReadAsAsync<MenuItem>();
+                list.Wait();
+                var menuItem = list.Result;
+                return new Cart(1, 1, menuItemId, menuItem.Name);
+            }
+        }
+    }
+}
+
---
+++ OrderItemWebApi/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderItemWebApi.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MenuItemId { get; set; }
+        public string MenuItemName { get; set; }
+
+        public Cart(int id, int userid, int menuitemid, string menuitemname)
+        {
+            this.Id = id;
+            this.UserId = userid;
+            this.MenuItemId = menuitemid;
+            this.MenuItemName = menuitemname;
+        }
+    }
+}
